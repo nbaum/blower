f7a3de97012f66a1ea42de82ddf872e10cd8d781
Nathan Baum <n@p12a.org.uk>
2015-11-04 03:15:54 +0000

    Process task directories.

--- lib/blower/context.rb
+++ lib/blower/context.rb
@@ -15,6 +15,7 @@ module Blower
     def initialize (path)
       @path = path
       @log = Logger.new
+      @have_seen = {}
     end
 
     def one_host (name = nil, &block)
@@ -33,11 +34,32 @@ module Blower
     end
 
     def run (task)
+      files = []
       @path.each do |dir|
         name = File.join(dir, task)
         name += ".rb" unless File.exist?(name)
-        fail "can't find #{task}" unless File.exist?(name)
-        return instance_eval(File.read(name), name)
+        if File.directory?(name)
+          dirtask = File.join(name, File.basename(@task))
+          dirtask += ".rb" unless File.exist?(dirtask)
+          name = dirtask
+          blowfile = File.join(name, "Blowfile")
+          files << blowfile if File.exist?(blowfile) && !@have_seen[blowfile]
+        end
+        files << name if File.exist?(name)
+        break unless files.empty?
+      end
+      if files.empty?
+        fail "can't find #{task}"
+      else
+        begin
+          old_task, @task = @task, task
+          files.each do |file|
+            @have_seen[file] = true
+            instance_eval(File.read(file), file)
+          end
+        ensure
+          @task = old_task
+        end
       end
     end
 
